7648ab2e0aa523c55f5d0b07be3c47f7ffa1d9c6
NEWVIRTUALPC\VirtualC <tomas.gabriel@sitewell.cz>
2025-04-30 15:55:15 +0200

    no longer detects language of browser

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -1,26 +1,22 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
-import Backend from 'i18next-http-backend';
-
 import translationsCs from './locales/cs/translation.json';
 import translationsEn from './locales/en/translation.json';
 
 i18n
-    .use(Backend)
-    .use(LanguageDetector)
     .use(initReactI18next)
     .init({
-        fallbackLng: 'en',
+        lng: 'cs',
+        fallbackLng: 'cs',
         debug: true,
         supportedLngs: ['en', 'cs'],
         interpolation: {
             escapeValue: false,
         },
         resources: {
-            en: { translation: translationsEn },
             cs: { translation: translationsCs },
-          },
+            en: { translation: translationsEn },
+        },
     });
 
 export default i18n;
